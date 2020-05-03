951b502d3542bc41a8164714e8704f236af3346d
Alexej Sommer <asommer@yandex.ru>
2020-05-03 10:36:53 +0300

    Jquery validation added

--- ViewModels/AddMessageViewModel.cs
+++ ViewModels/AddMessageViewModel.cs
@@ -1,16 +1,16 @@
 using System.ComponentModel.DataAnnotations;
-using System;
 
 namespace Guestbook.ViewModels
 {
 
     public class AddMessageViewModel
     {
-        public String Token { get; set; } 
+        public string Token { get; set; } 
+        [Required]
         public string SenderName { get; set; }
         [EmailAddress]
         public string Email { get; set; }
+        [Required]
         public string MessageText { get; set; }
     }
-
 }
